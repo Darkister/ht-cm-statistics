fb3cc36a1b7789b20553c00037776da952be2d7c
Marco Schneller <marco.schneller92@gmail.com>
2023-01-26 18:38:22 +0100

    refactore some functions to avoid a lot of iterations

--- Code.js
+++ Code.js
@@ -151,7 +151,7 @@ function getLatestValidPhase(phases){
 /** Checks the rest hp of the boss where the fight ends
  *  @param {String} json  fightData as json of the Encounter
  *  @param {String} boss  boss name where the encounter ends
- *  @return {String}      returns hp in percent as decimal
+ *  @return {Integer}      returns hp in percent as decimal
  */
 function bossHPendPhase(json, boss){
   var targets = json.targets;
@@ -530,6 +530,8 @@ function fillMechanics(){
         mechanicsValue[5 + (j * 10)][2 + (i * 3)] = mechanicsValue[5 + (j * 10)][0 + (i * 3)] < mechanicsValue[5 + ((j - 1) * 10)][0 + (i * 3)] ? "+" : mechanicsValue[5 + (j * 10)][0 + (i * 3)] == mechanicsValue[5 + ((j - 1) * 10)][0 + (i * 3)] ? "~" : mechanicsValue[5 + (j * 10)][1 + (i * 3)] == mechanicsValue[5 + ((j - 1) * 10)][1 + (i * 3)] ? "~" : "-";
         mechanicsValue[6 + (j * 10)][2 + (i * 3)] = mechanicsValue[6 + (j * 10)][0 + (i * 3)] < mechanicsValue[6 + ((j - 1) * 10)][0 + (i * 3)] ? "+" : mechanicsValue[6 + (j * 10)][0 + (i * 3)] == mechanicsValue[6 + ((j - 1) * 10)][0 + (i * 3)] ? "~" : mechanicsValue[6 + (j * 10)][1 + (i * 3)] == mechanicsValue[6 + ((j - 1) * 10)][1 + (i * 3)] ? "~" : "-";
       }
+
+      mechanicsValue[0 + (j * 10)][0 + (i * 3)] = "='Setup und Co'!B" + (2+i);
     }
   }
   mechanicsRange.setValues(mechanicsValue);
@@ -541,70 +543,34 @@ function fillMechanics(){
  * @param {Integer} days   [Optional] calculate only the last x days
  * @return {Integer}       returns a number
  */
-function getAmountOfMechanicFailes(range,player,days=-1){
-  var counter = 0;
-  if(days==-1){
-    for(var i = 0; i < range.length; i++){
-      counter += occurrences(range[i],player);
-    }
-  }
-  else{
-    var dates = logSheet.getRange(2,1,logSheet.getLastRow()-1,1).getValues(),
-        players = logSheet.getRange(2,8,logSheet.getLastRow()-1,10).getValues();
-    for(var i = dates.length - 1; i >= 0; i--){
-      if(days > 0){
-        if(dates[i][0] == ""){
-          for(var p = 0; p < players[i].length; p++){
-            if(players[i][p] == player){
-              counter += occurrences(range[i],player);
-              break;
-            }
-          }
-        }
-        else{
-          for(var p = 0; p < players[i].length; p++){
-            if(players[i][p] == player){
-              counter += occurrences(range[i],player);
-              days--;
-              break;
-            }
-          }          
-        }
+function getAmountOfMechanicFailes(range, player, days = -1) {
+  let counter = 0;
+  let dates = logSheet.getRange(2, 1, logSheet.getLastRow() - 1, 1).getValues();
+  let players = logSheet.getRange(2, 8, logSheet.getLastRow() - 1, 10).getValues();
+  let data = dates.map((date, index) => {
+      return {
+          date: date[0],
+          players: players[index],
+          range: range[index]
       }
-      else break;
-    }
+  });
+  if (days === -1) {
+      counter = data.reduce((acc, curr) => acc + occurrences(curr.range, player), 0);
+  } else {
+      counter = data
+          .filter(d => d.date === "" || days-- > 0)
+          .reduce((acc, curr) => acc + occurrences(curr.range, player), 0);
   }
-
   return counter;
 }
 
 /** Function that count occurrences of a substring in a string;
  * @param {String} string               The string
  * @param {String} subString            The sub string to search for
- * @param {Boolean} [allowOverlapping]  Optional. (Default:false)
- * 
- * @author Vitim.us https://gist.github.com/victornpb/7736865
- * @see Unit Test https://jsfiddle.net/Victornpb/5axuh96u/
- * @see https://stackoverflow.com/a/7924240/938822
  */
-function occurrences(string, subString, allowOverlapping) {
-
-  string += "";
-  subString += "";
-  if (subString.length <= 0) return (string.length + 1);
-
-  var n = 0,
-      pos = 0,
-      step = allowOverlapping ? 1 : subString.length;
-
-  while (true) {
-      pos = string.indexOf(subString, pos);
-      if (pos >= 0) {
-          ++n;
-          pos += step;
-      } else break;
-  }
-  return n;
+function occurrences(string, substring) {
+  let re = new RegExp(substring, "g");
+  return ((string + '').match(re) || []).length;
 }
 
 /** Calculate the average fails per try
@@ -614,39 +580,35 @@ function occurrences(string, subString, allowOverlapping) {
  * @param {Integer} days        [Optional] calculate only the last x days (Default:-1)
  * @return {Integer}            returns a number
  */
-function avgFailsPerTry(player,totalValue,phase,days=-1){
-  var allowedPhases = ["Purification 1","Jormag","Primordus","Kralkatorrik","Zeitzauberer der Leere","Purification 2","Mordremoth","Zhaitan","Void Saltspray Dragon","Purification 3","Soo-Won 1","Purification 4","Soo-Won 2"];
+function avgFailsPerTry(player, totalValue, phase, days = -1) {
+  const allowedPhases = ["Purification 1","Jormag","Primordus","Kralkatorrik","Zeitzauberer der Leere","Purification 2","Mordremoth","Zhaitan","Void Saltspray Dragon","Purification 3","Soo-Won 1","Purification 4","Soo-Won 2"];
+  const startIndex = allowedPhases.indexOf(phase);
+  const allowedPhasesFromPhase = allowedPhases.slice(startIndex);
+  const playerSet = new Set();
 
-  while(phase != allowedPhases[0]){
-    allowedPhases.shift();
-  }
-  var dates = logSheet.getRange(2,1,logSheet.getLastRow()-1,1).getValues(),
-      phases = logSheet.getRange(2,4,logSheet.getLastRow()-1,1).getValues(),
-      players = logSheet.getRange(2,8,logSheet.getLastRow()-1,10).getValues(),
-      counter = 0;
+  var counter = 0;
+  var lastRow = logSheet.getLastRow();
+  var date = logSheet.getRange(2,1,lastRow-1,1).getValues();
+  var phase = logSheet.getRange(2,4,lastRow-1,1).getValues();
+  var players = logSheet.getRange(2,8,lastRow-1,10).getValues();
 
-  if(days==-1){
-    for(var i = 0; i < phases.length; i++){
-      if(allowedPhases.includes(phases[i][0]) && players[i].includes(player)){
-        counter++;
-      }
+  for (let i = 0; i < date.length; i++) {
+    if (allowedPhasesFromPhase.includes(phase[i][0]) && players[i].includes(player)) {
+      playerSet.add(player)
+      counter++;
     }
   }
-  else{
-    for(var i = dates.length - 1; i >= 0; i--){
-      if(days > 0){
-        if(dates[i][0] == "" && allowedPhases.includes(phases[i][0]) && players[i].includes(player)){
-          counter++;
-        }
-        else if(allowedPhases.includes(phases[i][0]) && players[i].includes(player)){
-          counter++;
+
+  if(days !== -1){
+    for (let i = date.length-1; i >=0; i--) {
+      if (days > 0) {
+        if (date[i][0] !== "" && playerSet.has(players[i])) {
+          counter--;
           days--;
         }
-        else if(dates[i][0] != "" && players[i].includes(player)){
-          days--; 
-        }
+      } else {
+        break;
       }
-      else break;
     }
   }
   return totalValue/counter;
