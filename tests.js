bab8ef18ce26beedbb100b9649c3a3110b3c2b26
Marco Schneller <marco.schneller92@gmail.com>
2023-01-26 17:32:03 +0100

    add some tests

--- tests.js
+++ tests.js
@@ -1,31 +1,39 @@
-// if ((typeof GasTap)==='undefined') { // GasT Initialization. (only if not initialized yet.)
-//   eval(UrlFetchApp.fetch('https://raw.githubusercontent.com/huan/gast/master/src/gas-tap-lib.js').getContentText())
-// } // Class GasTap is ready for use now!
+if ((typeof GasTap)==='undefined') { // GasT Initialization. (only if not initialized yet.)
+    eval(UrlFetchApp.fetch('https://raw.githubusercontent.com/huan/gast/master/src/gas-tap-lib.js').getContentText())
+  } // Class GasTap is ready for use now!
+  
+var test = new GasTap()
+var json = apiFetch("https://dps.report/ICMq-20221123-211350_void")
+  
+function gast() {
+  
+  test('do deathCheck right', function (t) {    
+    var deathCheck = firstDeath(json);
+    t.equal(deathCheck, "blackicedragon.3579", 'deathCheck is ok');
+  })
+  
+  test('do fightDurationCheck right', function (t) {    
+    var duration = json.duration;
+    t.equal(duration,"06m 22s 687ms",'fightDurationCheck is ok');
+  })
+  
+  test('do endPhaseCheck right', function (t) {    
+    var endPhase = getLatestValidPhase(json.phases);
+    t.equal(endPhase, "Purification 2",'endPhaseCheck is ok');
+  })
+  
+  test('do occurrences right', function (t) {    
+    var counter = occurrences("Slayerstryke.6928Slayerstryke.6928Slayerstryke.6928Scharkoon.2017Scharkoon.2017Zailer.9817Zailer.9817Zailer.9817Zailer.9817","Slayerstryke.6928");
+    t.equal(counter, 3 ,'occurences is ok');
+  })
 
-// var test = new GasTap()
-
-// function gast() {
-
-//   test('do deathCheck right', function (t) {    
-//     var deathCheck = firstDeath("u4UF-20221216-205751_void",true);
-//     t.equal(deathCheck, "Domnorix Belphegor", 'deathCheck is ok');
-//   })
-
-//   test('do fightDurationCheck right', function (t) {    
-//     var duration = fightDuration("u4UF-20221216-205751_void");
-//     t.equal(duration,"08m 22s 222ms",'fightDurationCheck is ok');
-//   })
-
-//   test('do endPhaseCheck right', function (t) {    
-//     var endPhase = includesString("Jormag");
-//     t.equal(endPhase, "Jormag",'endPhaseCheck is ok');
-//   })
-
-//   test('do endPhaseCheck right', function (t) {    
-//     var endPhase = endPhase("u4UF-20221216-205751_void");
-//     t.equal(endPhase, "Zhaitan",'endPhaseCheck is ok');
-//   })
+  test('do occurrences right', function (t) {    
+    var counter = occurrences("Slayerstryke.6928stone goes perma.6980Slayerstryke.6928stone goes perma.6980GLD.7468GLD.7468Slayerstryke.6928stone goes perma.6980Scharkoon.2017stone goes perma.6980Scharkoon.2017Slayerstryke.6928stone goes perma.6980Scharkoon.2017Slayerstryke.6928Slayerstryke.6928Judy.8532stone goes perma.6980Homegrow.4365Scharkoon.2017Zailer.9817ToxicSkritt.6281Chelu.2095Chelu.2095Chelu.2095Chelu.2095Chelu.2095Chelu.2095Slayerstryke.6928Slayerstryke.6928stone goes perma.6980Slayerstryke.6928stone goes perma.6980Slayerstryke.6928Slayerstryke.6928Slayerstryke.6928Zailer.9817ToxicSkritt.6281Slayerstryke.6928ToxicSkritt.6281Slayerstryke.6928Zailer.9817Zailer.9817Homegrow.4365stone goes perma.6980ToxicSkritt.6281Slayerstryke.6928Judy.8532Scharkoon.2017Scharkoon.2017Judy.8532Slayerstryke.6928Scharkoon.2017Slayerstryke.6928Judy.8532stone goes perma.6980ToxicSkritt.6281Judy.8532stone goes perma.6980Slayerstryke.6928Judy.8532Slayerstryke.6928stone goes perma.6980Scharkoon.2017Judy.8532Slayerstryke.6928Scharkoon.2017Chelu.2095Chelu.2095Slayerstryke.6928Scharkoon.2017Judy.8532Judy.8532Scharkoon.2017Chelu.2095Slayerstryke.6928","Homegrow.4365");
+    t.equal(counter, 2 ,'occurences is ok');
+  })
+  
+  test.finish();
+}
 
 
-//   test.finish();
-// }
+
